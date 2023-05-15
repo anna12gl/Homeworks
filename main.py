8b145e7bc87556d8713c4a73eb7a9d340978775f
anna12gl <133417289+anna12gl@users.noreply.github.com>
2023-05-15 20:58:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+str = "my_favorite_songs= Waste Moment Staying, Alive, A Sorta Fairytale, Start Me Up, New Salvation"
+print (len(str))
+print(str [-93:-54])
+print(str [-93:-75] + str [-14:])
+print(str [-93:-75] + str [-53:-47])
+print(str [-93:-75] + str [-27:-15])
+
+
